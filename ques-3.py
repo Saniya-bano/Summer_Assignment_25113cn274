920578d76267d77d43955089ddc7c657b4053571
Saniya Bano <sbano3923@gmail.com>
2026-06-11 17:47:47 +0530

    Improve factorial calculation with input validation
    Add input validation for negative numbers and adjust indentation.

--- ques-3.py
+++ ques-3.py
@@ -1,6 +1,9 @@
 n=int(input("enter n:"))
-fact=1
-for i in range(0,n):
-    fact=fact*(i+1)
+if n<0:
+    print("not possible")
+else:    
+    fact=1
+    for i in range(0,n):
+        fact=fact*(i+1)
 
-print(fact)    
+     print(fact)    
