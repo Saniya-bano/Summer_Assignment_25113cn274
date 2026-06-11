81e4e0fc334139240e37838d26196d210f1e7ec5
Saniya Bano <sbano3923@gmail.com>
2026-06-11 15:27:07 +0530

    Refactor ques-1.py for clarity and consistency

--- ques-1.py
+++ ques-1.py
@@ -1,8 +1,8 @@
 n=int(input("enter n:"))
-# first method for sum of n numbers
+# first method for sum of n natural numbers
 sum=(n*(n+1))/2
 print(sum)
-# secondt method for sum of n numbers
+# secondt method for sum of n natural numbers
 sum=0
 for i in range(1,n+1):
   sum=sum+i
